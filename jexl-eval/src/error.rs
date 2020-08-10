7b3e21071e97ddd7dcb48cf5e505ac35ffd2726d
Tarik Eshaq <teshaq@mozilla.com>
2020-08-09 22:25:15 -0700

    Minor clean ups

---
+++ jexl-eval/src/error.rs
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use jexl_parser::{ast::OpCode, ParseError, Token};
+
+use serde_json::Value;
+
+pub type Result<'a, T, E = EvaluationError<'a>> = std::result::Result<T, E>;
+#[derive(Debug, thiserror::Error)]
+pub enum EvaluationError<'a> {
+    #[error("Parsing error: {0}")]
+    ParseError(Box<ParseError<usize, Token<'a>, &'a str>>),
+    #[error("Invalid binary operation, left: {left}, right: {right}, operation: {operation}")]
+    InvalidBinaryOp {
+        left: Value,
+        right: Value,
+        operation: OpCode,
+    },
+    #[error("Unknown transform: {0}")]
+    UnknownTransform(String),
+    #[error("Duplicate object key: {0}")]
+    DuplicateObjectKey(String),
+    #[error("Invalid context provided")]
+    InvalidContext,
+    #[error("Invalid index type")]
+    InvalidIndexType,
+    #[error("Invalid json: {0}")]
+    JSONError(#[from] serde_json::Error),
+}
+
+impl<'a> From<ParseError<usize, Token<'a>, &'a str>> for EvaluationError<'a> {
+    fn from(cause: ParseError<usize, Token<'a>, &'a str>) -> Self {
+        EvaluationError::ParseError(Box::new(cause))
+    }
+}
--- jexl-parser/src/ast.rs
+++ jexl-parser/src/ast.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #[derive(Debug, PartialEq)]
 pub enum Expression {
     Number(f64),
@@ -31,4 +35,31 @@ pub enum OpCode {
     Modulus,
     Exponent,
     In,
+}
+
+impl std::fmt::Display for OpCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                OpCode::Add => "Add",
+                OpCode::Subtract => "Subtract",
+                OpCode::Multiply => "Multiply",
+                OpCode::Divide => "Divide",
+                OpCode::FloorDivide => "Floor division",
+                OpCode::Less => "Less than",
+                OpCode::LessEqual => "Less than or equal to",
+                OpCode::Greater => "Greater than",
+                OpCode::GreaterEqual => "Greater than or equal to",
+                OpCode::Equal => "Equal",
+                OpCode::NotEqual => "Not equal",
+                OpCode::And => "Bitwise And",
+                OpCode::Or => "Bitwise Or",
+                OpCode::Modulus => "Modulus",
+                OpCode::Exponent => "Exponent",
+                OpCode::In => "In",
+            }
+        )
+    }
 }
